1b387ad34a35f2c3ef58b59bbf826307532ed880
calcala <c.alcala@uniandes.edu.co>
2020-10-30 19:35:07 -0500

    build

---
+++ build/precache-manifest.6e394f70b2291b8c1e4cff03a7ce5cf6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e063cae46732adf5d530d867ac5030a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0807b8c33a4561e38ecb",
+    "url": "/static/css/main.7c00ca9d.chunk.css"
+  },
+  {
+    "revision": "d13540a0d728be96f020",
+    "url": "/static/js/2.51e106fd.chunk.js"
+  },
+  {
+    "revision": "b725272cef1706f68cd4f5229485ee55",
+    "url": "/static/js/2.51e106fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0807b8c33a4561e38ecb",
+    "url": "/static/js/main.dbeb6544.chunk.js"
+  },
+  {
+    "revision": "577e22ed82775880d393",
+    "url": "/static/js/runtime-main.d81bd5bf.js"
+  },
+  {
+    "revision": "7407b19facd8c2132b6eee3af9551e0b",
+    "url": "/static/media/bioshock-back.7407b19f.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b930aa8ff29f7e6f6f92f3e3e3b18fed",
+    "url": "/static/media/red-dead.b930aa8f.jpg"
+  }
+]);
